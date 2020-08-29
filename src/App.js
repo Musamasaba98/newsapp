f952b251801441e272a30cd3500ccf975d4a8d1d
Masaba Musa <masamata98@gmail.com>
2020-08-29 11:25:04 -0700

    ...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { TwitterShareButton, TwitterIcon } from "react-share";
+import "./App.css";
 
 function App() {
+  const [article, setArticle] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?sources=the-next-web&apiKey=bceecf84217244119ff3f819b2fdd08f"
+      )
+      .then((res) => {
+        setArticle(res.data.articles);
+      });
+
+    return () => {};
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="card-log">
+        {article.splice(0, 3).map((article) => {
+          return (
+            <div key={article.id} className="card">
+              <h3>{article.title}</h3>
+              <img src={article.urlToImage} alt="print" />
+              <p>{article.author}</p>
+              <TwitterShareButton title={article.title} url={article.url}>
+                <TwitterIcon size={32} round />
+              </TwitterShareButton>
+              <i class="fas fa-share"></i>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
